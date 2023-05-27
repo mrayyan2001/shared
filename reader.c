7d68490585ff81a0b9647d01fec21d3adc89c91c
mryyan2001 <mmrian2001@gmail.com>
2023-05-27 21:23:49 +0300

    first commit

---
+++ reader.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+
+#define SHM_SIZE 1024
+
+int main(int argc, char *argv[])
+{
+
+    key_t key = ftok("/tmp", 0);
+    if (key == -1)
+    {
+        perror("ftok");
+        exit(1);
+    }
+
+    int shmid = shmget(key, SHM_SIZE, 0666);
+    if (shmid == -1)
+    {
+        perror("shmget");
+        exit(1);
+    }
+
+    char *shmaddr = shmat(shmid, NULL, 0);
+
+    printf("%s\n", shmaddr);
+
+    if (shmdt(shmaddr) == -1)
+    {
+        perror("shmdt");
+        exit(1);
+    }
+
+    return 0;
+}
